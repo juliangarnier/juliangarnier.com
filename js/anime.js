fb38b16d174c53176870cfdf42cdf0be3a3b7423
Julian Garnier <julian.garnier@gmail.com>
2024-11-11 16:02:28 +0100

    Update comment description

--- js/anime.js
+++ js/anime.js
@@ -1,7 +1,7 @@
 /*
  * Anime v0.0.0
  * http://anime-js.com
- * Javascript animation library
+ * JavaScript animation library
  * Copyright (c) 2016 Julian Garnier
  * http://juliangarnier.com
  * Released under the MIT license
